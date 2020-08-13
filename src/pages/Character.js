47ba3d57226288ded9bed33592fad9c949fb54e6
Jesus Hernandez <hdmjesus@gmail.com>
2020-08-13 15:55:20 -0400

    templates: home, character. ,Realizados

---
+++ src/pages/Character.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-tabs */
+const Character = () => {
+  const view = `/
+	<div class="Character-inner>
+		<article class="character-card">
+				<img src="image" alt="name">
+				<h2>Name</h2>
+		</article>
+
+		<article class="Character-card>
+			<h3> Episodios:</h3>
+			<h3> Estado:</h3>
+			<h3> Especie:</h3>
+			<h3> Genero:</h3>
+			<h3> Origen:</h3>
+			<h3> Ultima ubicacion:</h3>
+		</article>
+	</div>
+`;
+
+  return view;
+};
+
+export default Character;
---
+++ src/templates/Header.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-tabs */
+const Header = () => {
+  const view = `
+		<div class="Header-main">
+			<div class="Header-logo">
+				<h1>
+					<a href="/">
+							Rick and Morty
+					</a>
+				</h1>
+			</div>
+		</div>
+		<div class="Header-nav">
+			<a href="#/about/>
+			About
+			</a>
+		</div>
+`;
+
+  return view;
+};
+
+export default Header;
